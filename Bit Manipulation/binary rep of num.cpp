3ae6d556b95b755e83c7f5418b575f8eee5846de
jayasree <jayas8sree@gmail.com>
2018-01-24 21:08:01 +0530

    commit-2-20180124-2107
    Binary representation of a number

---
+++ Bit Manipulation/binary rep of num.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void bin(int n)
+{
+	if(n>1)
+	bin(n/2);
+	cout<<n%2;
+}
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	bin(n);
+	
+	return 0;
+}
